c09636c9103c70e9f4658ace8d3ef14d274eaeb3
Cameron Currie <me@cameroncurrie.net>
2017-10-23 20:51:50 -0500

    Add fresh project

---
+++ Identicon Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Identicon Test
+//
+//  Created by Cameron Currie on 10/23/17.
+//  Copyright © 2017 Example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
